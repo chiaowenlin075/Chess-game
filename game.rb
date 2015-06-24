258b728feb3829ccc5d33f3865dd906879923d9c
Leah Lin <chiaowenlin075@gmail.com>
2015-06-24 19:29:13 +0800

    Implement different play mode, either single human player or 2 human players.

--- game.rb
+++ game.rb
@@ -3,9 +3,10 @@ class Game
   attr_accessor :current_player
 
   def initialize
-    @board = Board.new
-    @player1 = ComputerPlayer.new(board, :white)
-    @player2 = HumanPlayer.new(board, :black)
+    initial_cond = decide_players
+    @board = initial_cond[:board]
+    @player1 = initial_cond[:player1]
+    @player2 = initial_cond[:player2]
     @current_player = @player1
   end
 
@@ -19,6 +20,23 @@ class Game
       end
       self.current_player = current_player == @player1 ? @player2 : @player1
     end
+  end
+
+  def decide_players
+    puts "Type 1 for single player against computer, 2 for 2 players: "
+    num_of_players = gets.chomp.to_i
+
+    initial_cond = {}
+    board = Board.new
+    initial_cond[:board] = board
+    initial_cond[:player1] = HumanPlayer.new(board, :white)
+    if num_of_players == 1
+      initial_cond[:player2] = ComputerPlayer.new(board, :black)
+    else
+      initial_cond[:player2] = HumanPlayer.new(board, :black)
+    end
+
+    initial_cond
   end
 
 end
